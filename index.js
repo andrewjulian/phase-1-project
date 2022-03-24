054c1daa3e716a29ca9df383dbbec57dd23ccd2d
+ <+>
2022-03-24 10:36:07 -0600

    comments added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', () => { 
 
+    //get the pose data from the db
     fetch("http://localhost:3000/poses")
     .then(response => response.json())
     .then(data => poseOptionDisplay(data))
 
+    //create the english pose name options under quick search dropdown
     function poseOptionDisplay(data){
         const dropdownList = document.getElementById("nameDropDown")
         for(let objKey in data){
@@ -14,4 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
             dropdownList.appendChild(li)
         }
     }
+
+    
+
+
 })
