4e3a69ea1ebebb42c45a277eca6558fe185aedcc
+ <+>
2022-03-24 10:28:55 -0600

    add english names of poses to quick search drop down (js file)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', () => { 
 
-    document.querySelector("#nameSearch").addEventListener("change", findPose)
+    fetch("http://localhost:3000/poses")
+    .then(response => response.json())
+    .then(data => poseOptionDisplay(data))
 
-    function findPose(){
-        fetch("http://localhost:3000/poses")
-        .then(response => response.json())
-        .then(data => console.log(data))   //display pose function instead of console.log
+    function poseOptionDisplay(data){
+        const dropdownList = document.getElementById("nameDropDown")
+        for(let objKey in data){
+            const li = document.createElement("li")
+            li.classList.add("dropdown-menu-item")
+            li.textContent = data[objKey].english_name
+            dropdownList.appendChild(li)
+        }
     }
-
-    function displayPose(data){
-        
-    }
-
-
 })
