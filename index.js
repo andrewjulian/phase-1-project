1b0faefb365b30a263f2b30d22db1fd198888c5a
+ <+>
2022-03-24 12:38:05 -0600

    review and revision with MP

--- index.js
+++ index.js
@@ -2,31 +2,33 @@
 document.addEventListener('DOMContentLoaded', () => { 
 
     //get the pose data from the db
+    let poses = []
     fetch("http://localhost:3000/poses")
     .then(response => response.json())
-    .then(data => poseOptionDisplay(data))
+    .then(data => {
+        poses = data
+        poseOptionDisplay(poses)
+    })
 
     //create the english pose name options under quick search dropdown
     function poseOptionDisplay(data){
         const dropdownList = document.getElementById("nameDropDown")
         for(let objKey in data){
-            const button = document.createElement("button")
-            button.classList.add("dropdown-menu-item")
-            button.setAttribute("type", "button")
-            button.textContent = data[objKey].english_name
-            dropdownList.appendChild(button)
+            const li = document.createElement("li")
+            li.classList.add("dropdown-menu-item")
+            li.textContent = data[objKey].english_name
+            dropdownList.appendChild(li)
         }
     }
 
     //display pose based on selection from drop down
-    const dropDownButton = document.getElementById("nameDropDown")
+    const dropDownButton = document.getElementById("dropdownMenuButton2")
     console.log("Drop Down Text Content: ", dropDownButton)
-    dropDownButton.addEventListener("change", () => {
+    dropDownButton.addEventListener("change", (e) => {
         console.log("event working!")
-        console.log(dropDownButton.textContent)
-        return dropDownButton.textContent
+        console.log(e.target.value)
+        return e.target.value
     })
-    
 
 
 })
